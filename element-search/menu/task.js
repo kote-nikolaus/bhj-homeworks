ab63e2f69920eab703f86c5499c626a74ccbbffe
Katya Nikolaeva <nikolaus12@yandex.ru>
2020-08-02 18:53:53 +0300

    Second task updated

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,13 +3,13 @@
 let links = Array.from(document.getElementsByClassName('menu__link'));
 
 function openLink() {
-  let activeMenu = document.getElementsByClassName('menu_active').item(0);
-  if (activeMenu) {
-    activeMenu.classList.remove('menu_active');
-  }
   let menuSub = this.closest('li').querySelector('li ul.menu_sub');
   if (menuSub !== null) {
-    menuSub.classList.add('menu_active');
+    if (menuSub.classList.contains('menu_active')) {
+        menuSub.classList.remove('menu_active');
+    } else {
+      menuSub.classList.add('menu_active');
+    }
     return false;
   }
   return true;
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -6,47 +6,35 @@ let rightArrow = document.getElementsByClassName('slider__arrow_next').item(0);
 let slides = Array.from(document.getElementsByClassName('slider__item'));
 let activeSlide = document.querySelector('.slider__item_active');
 
-function scrollRight() {
-  let newActiveSlide = slides[(slides.indexOf(activeSlide) + 1) % slides.length];
+let dots = Array.from(document.getElementsByClassName('slider__dot'));
+let activeDot = document.querySelector('.slider__dot_active');
+
+function changeActiveSlide(index) {
+  let newActiveSlide = slides[index];
   activeSlide.classList.remove('slider__item_active');
   newActiveSlide.classList.add('slider__item_active');
   activeSlide = newActiveSlide;
 
-  let newActiveDot = dots[slides.indexOf(activeSlide)];
+  let newActiveDot = dots[index];
   activeDot.classList.remove('slider__dot_active');
   newActiveDot.classList.add('slider__dot_active');
   activeDot = newActiveDot;
 }
 
-function scrollLeft() {
-  let newActiveSlide = slides[(slides.indexOf(activeSlide) - 1 + slides.length) % slides.length];
-  activeSlide.classList.remove('slider__item_active');
-  newActiveSlide.classList.add('slider__item_active');
-  activeSlide = newActiveSlide;
+function scrollRight() {
+  changeActiveSlide((slides.indexOf(activeSlide) + 1) % slides.length);
+}
 
-  let newActiveDot = dots[slides.indexOf(activeSlide)];
-  activeDot.classList.remove('slider__dot_active');
-  newActiveDot.classList.add('slider__dot_active');
-  activeDot = newActiveDot;
+function scrollLeft() {
+  changeActiveSlide((slides.indexOf(activeSlide) - 1 + slides.length) % slides.length);
+}
+
+function showSlide() {
+  changeActiveSlide(dots.indexOf(this));
 }
 
 rightArrow.onclick = scrollRight;
 leftArrow.onclick = scrollLeft;
-
-let dots = Array.from(document.getElementsByClassName('slider__dot'));
-let activeDot = document.querySelector('.slider__dot_active');
-
-function showSlide() {
-  let newActiveDot = this;
-  activeDot.classList.remove('slider__dot_active');
-  newActiveDot.classList.add('slider__dot_active');
-  activeDot = newActiveDot;
-
-  let newActiveSlide = slides[dots.indexOf(activeDot)];
-  activeSlide.classList.remove('slider__item_active');
-  newActiveSlide.classList.add('slider__item_active');
-  activeSlide = newActiveSlide;
-}
 
 for (let i = 0; i < dots.length; i++) {
   dots[i].onclick = showSlide;
